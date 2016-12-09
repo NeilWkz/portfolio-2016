d11cfb4f25bfb616d79ca06da4794bfe2925f275
NeilVGD <neil@vgd.uk.com>
2016-12-09 21:46:02 +0000

    import partials to cv page

---
+++ cv.php
@@ -0,0 +1,163 @@
+<?php include "partials/head.php"; ?>
+
+<body class="brdr-black">
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class=" col-xs-12 col-sm-4 bottom">
+                    <div id="logo" class="marquee">
+                        <h1><a href="index.php?cv=true">
+                        <!-- words span wrapped for control -->
+                            <span class="n">Neil</span>
+                            <span class="r">Ross</span>
+                            <span class="f">Frontend</span>
+                            <span class="d">Dev.</span>
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-sm-offset-2 co middle">
+                    <nav>
+                        <ul class="row between-xs">
+                           <!--  <li class="col-xs"><a href="#">Portfolio</a></li> -->
+                            <li class="col-xs"><a href="cv.html">Curriculum Vitae</a></li>
+                            <li class="col-xs"><a href="index.php?cv=true&contact=true">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+        <div class="brdr-box"></div>
+    </header>
+
+    <main class=" content">
+    <article>
+        <div class="wrap container-fluid">
+        <div class="row cv-head">
+            <div class="col-sm-6 col-md-5 col-md-offset-1">
+                <h1>Curriculum Vitae</h1>
+            </div>
+            <!-- /.col-sm-4 -->
+            <div class="col-sm-6 print">
+                <a class="button print-btn" href="javascript:window.print()">Print This Page</a>
+            </div>
+            <div class="col-sm-6 cv-deets">
+                <div class="row">
+                    <div class="col-sm-6 cv-cell">
+                        <p>Neil Ross</p>
+                    </div>
+                    <div class="col-sm-6 cv-cell">
+                        <p>www.wkzdesign.co.uk</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 cv-cell">
+                        <p><span class="dummy">Hooja</span>7<span class="dummy">sdakljsadl29</span>1<span class="dummy">Boo</span> By<span class="dummy">dfsf54dsf</span>ron<span class="dummy">Josta</span> Ro<span class="dummy">5a4ds65</span>ad<span class="dummy">doo doo</span></p>
+                    </div>
+                    <div class="col-sm-6 cv-cell">
+                        <p><span class="dummy">)*(*)&KFH</span>(44)<span class="dummy">1dsf6vds12v</span> 7919<span class="dummy">dfsf4546d5s3</span> 354<span class="dummy">32234tegfsdf3</span> 716<span class="dummy">3544rwe6rtf</span></p>
+                    </div>
+                </div> <!-- /row -->
+                <div class="row">
+                    <div class="col-sm-8 cv-cell cv-btm">
+                        <p>London</p>
+                    </div>
+                    <!-- /.col-sm-8 -->
+                    <div class="col-sm-2 cv-cell"><p><span class="dummy">dfs416vsd</span>E<span class="dummy">234q4fsd3</span>1<span class="dummy">vdsvsd254</span>7</p><span class="dummy">fassaf5</span></div>
+                    <!-- /.col-sm-2 -->
+                    <div class="col-sm-2 cv-cell"><p>4SN</p></div>
+                    <!-- /.col-sm-2 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.col-sm-7 -->
+        </div>
+            <div class="row">
+                <div class="col-sm-8 col-md-6 col-md-offset-1 ">
+                <div class="cv-col cv-left">
+                    
+               
+                    <h3 class="col-header">Employment</h3>
+                    <div class="job">
+                        <h4 class="timeframe">May 2014 - Present Day</h4>
+                        <h5 class="title">Frontend Developer</h5>
+                        <p class="employer"><a href="http://www.vgd.uk.com">VGD</a></p>
+                        <p>I worked as the agency's primary frontend developer. VGD produce marketing sites, online booking systems and CRMs. I worked between Designers, Backend Developers &amp; clients to deliver responsive websites and applications, and supported Junior Developers in the delivery of their projects.</p>
+
+
+                    </div>
+                    <div class="job">
+                        <h4 class="timeframe">April 2013 – May 2014</h4>
+                        <h5 class="title">Front-End Developer</h5>
+                        <p class="employer"><a href="https://senshi.digital/">Dojo Design Studio (now Senshi.digital)</a></p>
+                        <p>Working with the studio's Design directors I was responsible for the HTML/CSS &amp; Javascript development of a number of web projects. The agency specialised in using Concrete5 to produce brochure quality marketing sites.</p>
+                    </div>
+                    <div class="job">
+                        <h4 class="timeframe">October 2010 - April 2013</h4>
+                        <h5 class="title">Freelance Web Developer/Designer</h5>
+                        <p class="employer"><a href="http://www.wkzdesign.co.uk/legacy">www.wkzdesign.co.uk/legacy</a></p>
+                        <p>I designed &amp; developed a number of freelance projects, most have now completed their lifecycle, to get an idea of my work please browse my legacy freelance <a href="http://www.wkzdesign.co.uk/legacy">portfolio site</a>. I provided day-to-day support and development for
+                            a wide range of clients &amp; also did frontend on a number of white label projects on contract for web agencies.</p>
+                    </div>
+                    <div class="job">
+                        <h4 class="timeframe">October 2006 – October 2007</h4>
+                        <h5 class="title">Web Admistrator</h5>
+                        <p class="employer"><a href="http://sinewavedesign.com/">Sinewave Design</a></p>
+                        <p>Sinewave Design produced music industry websites for high profile artists. My duties included assisting in building out new site designs in HTML and Flash, Photoshopping Images for use on existing sites and coding promotional emails.</p>
+
+                    </div>
+
+                     </div>
+                <!-- /.cv-col -->
+
+
+                </div>
+                <!-- /.col-sm-7 -->
+                <div class=" col-sm-4 col-md-4 col-md-offset-1 first-xs last-sm col-xs-12">
+                <div class="cv-col cv-right">
+                    <h3 class="col-header">Professional Skills</h3>
+                    <h4 class="top">Technologies</h4>
+                    <ul>
+                        <li>CSS (preprocessed with SASS or LESS)</li>
+                        <li>HTML</li>
+                        <li>Javascript</li>
+                        <li>PHP (templating)</li>
+                        <li>Responsive HTML Email</li>
+                        <li>Adobe Creative Suite</li>
+                        <li>Sketch</li>
+                        <li>Git</li>
+
+                    </ul>
+                    <h4>I've delivered with:</h4>
+                    <ul>
+                        <li>Bootstrap</li>
+                        <li>Wordpress</li>
+                        <li>Concrete5</li>
+                        <li>Symfony</li>
+                        <li>Opencart</li>
+                        <li>Zend2</li>
+                        <li>Angular</li>
+                        <li>Handlebars</li>
+                    </ul>
+                    </div>
+                <!-- /.cv-col -->
+                </div>
+                <!-- /.col-sm-5 -->
+            </div>
+        </div>
+        <!-- /.wrap container-fluid -->
+        </article>
+    </main>
+
+<?php include "partials/footer.php"; ?>
+
+   
+    <!-- Scripts Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="javascripts/ie10.js"></script>
+    <script src="javascripts/main.js"></script>
+</body>
+
+</html>

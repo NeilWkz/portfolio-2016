9bd650a28f1528b443bce4d30d2c48c7867be314
NeilVGD <neil@vgd.uk.com>
2016-12-09 22:16:27 +0000

    adding templating boilerplate

---
+++ boilerplate.php
@@ -0,0 +1,51 @@
+<?php include "partials/head.php"; ?>
+
+<body class="brdr-black">
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class=" col-xs-12 col-sm-4 bottom">
+                    <div id="logo" class="marquee">
+                        <h1><a href="index.php?played=true">
+                        <!-- words span wrapped for control -->
+                            <span class="n">Neil</span>
+                            <span class="r">Ross</span>
+                            <span class="f">Frontend</span>
+                            <span class="d">Dev.</span>
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-sm-offset-2 co middle">
+                    <nav>
+                        <ul class="row between-xs">
+                           <!--  <li class="col-xs"><a href="#">Portfolio</a></li> -->
+                            <li class="col-xs"><a href="cv.html">Curriculum Vitae</a></li>
+                            <li class="col-xs"><a href="index.php?played=true&contact=true">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+        <div class="brdr-box"></div>
+    </header>
+
+    <main class="content">
+    <article>
+        <div class="wrap container-fluid">
+        </div> <!-- /wrap container-fluid -->
+        </article>
+    </main>
+
+<?php include "partials/footer.php"; ?>
+
+   
+    <!-- Scripts Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="javascripts/ie10.js"></script>
+    <script src="javascripts/main.js"></script>
+</body>
+
+</html>

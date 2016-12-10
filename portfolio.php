4a912c28332534ba68fd5297eb952365ed498869
NeilVGD <neil@vgd.uk.com>
2016-12-10 13:11:13 +0000

    adding portfolio list layout

--- portfolio.php
+++ portfolio.php
@@ -32,7 +32,7 @@
     </header>
 
     <main class="content">
-    <article>
+    <section class="portfolio">
         <div class="wrap container-fluid">
         <div class="row">
             <div class="col-sm-12 col-md-10 col-md-offset-1">
@@ -42,25 +42,69 @@
         </div>
         <!-- /.row -->
         <div class="row">
-            <div class="col-sm-6 col-md-5 col-md-offset-1">
+            <div class="col-xs-12 col-sm-6 col-md-5 col-md-offset-1">
                  <article class="portfolio-entry">
-                     <img src="http://lorempixel.com/400/200" />
-                     <div class="entry-info"></div>
+                     <img src="http://lorempixel.com/360/440" />
+                     <div class="entry-info">
+                         <h3>Rumble Films</h3>
+                         <p>
+                             A custom wordpress site with ajax video gallery, and preloaded GIF rollovers.
+                         </p>
+                         <a href="http://www.werumble.com/"></a>
+                     </div>
                      <!-- /.entry-info -->
                  </article>
                  <!-- /.portfolio-entry -->
             </div>
             <!-- /.col-sm-6 col-md-5 col-md-offset-1 -->
-            <div class="col-sm-6 col-md-5">
+            <div class="col-xs-12 col-sm-6 col-md-5">
                  <article class="portfolio-entry">
-                     <img src="http://lorempixel.com/400/200" />
-                     <div class="entry-info"></div>
+                     <img src="http://lorempixel.com/360/440" />
+                     <div class="entry-info">
+                         <h3>Left Luggage</h3>
+                         <p>
+                             At enim sequor utilitatem. Sed erat aequius Triarium aliquid de dissensione nostra iudicare.  
+                         </p>
+                         <a href="#"></a>
+                     </div>
                      <!-- /.entry-info -->
                  </article>
                  <!-- /.portfolio-entry -->
             </div>
             <!-- /.col-sm-6 col-md-5 -->
-        </div>
+        </div> <!-- /row -->
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-5 col-md-offset-1">
+                 <article class="portfolio-entry">
+                     <img src="http://lorempixel.com/360/440" />
+                     <div class="entry-info">
+                         <h3>Rumble Films</h3>
+                         <p>
+                             A custom wordpress site with ajax video gallery, and preloaded GIF rollovers.
+                         </p>
+                         <a href="http://www.werumble.com/"></a>
+                     </div>
+                     <!-- /.entry-info -->
+                 </article>
+                 <!-- /.portfolio-entry -->
+            </div>
+            <!-- /.col-sm-6 col-md-5 col-md-offset-1 -->
+            <div class="col-xs-12 col-sm-6 col-md-5">
+                 <article class="portfolio-entry">
+                     <img src="http://lorempixel.com/360/440" />
+                     <div class="entry-info">
+                         <h3>Left Luggage</h3>
+                         <p>
+                             At enim sequor utilitatem. Sed erat aequius Triarium aliquid de dissensione nostra iudicare.  
+                         </p>
+                         <a href="#"></a>
+                     </div>
+                     <!-- /.entry-info -->
+                 </article>
+                 <!-- /.portfolio-entry -->
+            </div>
+            <!-- /.col-sm-6 col-md-5 -->
+        </div><!-- /row -->
         </div> <!-- /wrap container-fluid -->
         </article>
     </main>
